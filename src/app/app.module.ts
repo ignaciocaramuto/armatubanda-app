8594cb01243e7c7cf73d4d2a72ce9f7b2726b730
Nacho <ignaciocaramuto@gmail.com>
2022-04-21 14:09:37 -0300

    Feat: implementado header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { PrimengModule } from './primeng/primeng.module';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
