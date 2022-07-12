2bda73ebe190a4d731648e3e4d93a489d503b062
Nacho <ignaciocaramuto@gmail.com>
2022-07-12 15:42:03 -0300

    update: nuevo componente 'perfil' + ruta /perfil

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ListadoComponent } from './listado/listado.component';
 import { LoginComponent } from './login/login.component';
+import { PerfilComponent } from './perfil/perfil.component';
 import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
@@ -21,6 +22,10 @@ const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent
+  },
+  {
+    path: 'perfil',
+    component: PerfilComponent
   },
   {
     path: '**',
