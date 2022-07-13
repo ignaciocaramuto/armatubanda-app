8ab124864da31bf486ea06d64717a08d3fe582af
Nacho <ignaciocaramuto@gmail.com>
2022-07-13 17:06:40 -0300

    update: creacion preliminar de perfil

--- src/app/perfil/perfil.component.ts
+++ src/app/perfil/perfil.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Usuario } from '../listado/usuario.entities';
+import { PerfilService } from './perfil.service';
 
 @Component({
   selector: 'app-perfil',
@@ -9,11 +11,15 @@ import { ActivatedRoute, Params } from '@angular/router';
 export class PerfilComponent implements OnInit {
 
   idUsuario: number;
+  usuario: Usuario;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private servicioPerfil: PerfilService) { }
 
   ngOnInit(): void {
     this.route.params.subscribe((params: Params) => this.idUsuario = params['id']);
+    this.servicioPerfil.getUsuario(this.idUsuario).subscribe(usuario => {
+      this.usuario = usuario;
+    });
   }
 
 }
